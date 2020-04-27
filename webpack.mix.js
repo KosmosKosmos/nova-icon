70888a6071833f0a7ab0e17d5d0a52d5d1742ee4
Andreas Kosmowicz <andreas@kosmoskosmos.de>
2020-04-27 16:31:25 +0200

    v0.26.3 testing: - [FIX] Missing translation - [UPDATE] Rating field will never ever appear in form anymore (only on index and detail)

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,3 +3,10 @@ let mix = require('laravel-mix')
 mix.setPublicPath('dist')
     .js('resources/js/field.js', 'js')
     .sass('resources/sass/field.scss', 'css')
+
+
+module.exports = {
+    configureWebpack: {
+        devtool: 'nosources-source-map',
+    }
+}
